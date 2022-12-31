5147e5788c9de32ae452db49e95a4bc07b5ce1e9
Ronnel Santiago <ronnel.santiago@gmail.com>
2022-12-31 12:09:20 +1100

    Baseline work

--- README.md
+++ README.md
@@ -1,1 +1,112 @@
-# new-win11-machine
+# ❓What the heck is this
+
+Install apps and preliminary setup mainly for dev work on a new `Windows 11(22H2)` machine(bare metal or VM).
+
+>For your own sanity please review the pwsh script!!!
+
+This script will:
+
+- Install WSL 2
+  - Ubuntu and Kali
+- Installs apps primarily using `winget`
+  - See [winget-packages][wingetpac] for list of apps to be installed
+- Installs apps from `chocolatey` because they are not on `winget` yet
+- Installs pwsh modules
+- Development tools
+- Enabling Windows Features
+- Setup few basic git alias
+- Downloads Cascadia Nerd Font, install is manual for now.
+- Removes pre-loaded apps that I don't need
+- Creates folders
+  - repos🧑‍💻, games🎮, temp
+
+![alt text](output.png)
+
+> On a descent machine and internet connection this usually takes around 35-45 minutes.
+
+## 🔰Gettings started
+
+Prerequisites:
+
+- `Windows PowerShell 5.1`, this is the installed version on `Win11-22H2`
+- `winget`, do a Windows Store App update first so you got the latest, tested on `v1.3`
+
+Fire up an elevated powershell and run:
+
+```pwsh
+iwr -Uri 'https://raw.githubusercontent.com/ronnelsantiago/new-win11-machine/main/winget-packages.json' -OutFile winget-packages.json; iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/ronnelsantiago/new-win11-machine/main/new-win11.ps1'))
+```
+
+Running it will:
+
+- download the `winget-packages.json` file
+- run the pwsh script `new-win11.ps1`
+
+## 🧑‍💻Apps to be installed
+
+```text
+7zip.7zip
+Amazon.AWSCLI
+Atlassian.Sourcetree
+baretail
+BleachBit.BleachBit
+Brave.Brave
+Canonical.Ubuntu.2204
+Chocolatey.ChocolateyGUI
+CoreyButler.NVMforWindows
+Docker.DockerDesktop
+Git.Git
+GitHub.cli
+GitHub.GitHubDesktop
+GoLang.Go.1.19
+Google.Chrome
+JanDeDobbeleer.OhMyPosh
+JetBrains.Toolbox
+kalilinux.kalilinux
+LINQPad.LINQPad.7
+Microsoft.AzureCLI
+Microsoft.AzureCosmosEmulator
+Microsoft.AzureDataStudio
+Microsoft.AzureStorageEmulator
+Microsoft.AzureStorageExplorer
+Microsoft.Edge.Dev
+Microsoft.Office
+Microsoft.OpenJDK.17
+Microsoft.PowerShell
+Microsoft.SQLServerManagementStudio
+Microsoft.Teams
+Microsoft.VisualStudio.2022.Community
+Microsoft.VisualStudio.2022.Professional
+Microsoft.VisualStudioCode
+Microsoft.VisualStudioCode.Insiders
+Microsoft.WindowsTerminal.Preview
+Mozilla.Firefox
+NickeManarin.ScreenToGif
+Notepad++.Notepad++
+OpenWhisperSystems.Signal
+paint.net
+Postman.Postman
+PuTTY.PuTTY
+REALiX.HWiNFO
+RescueTime.DesktopApp
+SlackTechnologies.Slack
+smtp4dev
+Sysinternals Suite
+Telerik.Fiddler.Classic
+VMware.WorkstationPlayer
+Yarn.Yarn
+Yubico.YubikeyManager
+```
+
+## 📚References
+
+- https://github.com/tathamoddie/New-Machine.ps1
+- https://github.com/matt9ucci/DockerCompletion
+- https://github.com/PowerShell/PSReadLine/blob/master/PSReadLine/SamplePSReadLineProfile.ps1#L13-L21
+
+### 🗒️Notes
+
+- PSReadline now installed by default on pwsh 7.3
+- `winget export -o myExportedWingetApps.json`
+
+[wingetpac]: /winget-packages.json
---
+++ new-win11.ps1
@@ -0,0 +1,189 @@
+[CmdletBinding()]
+param ()
+
+$timer = [System.Diagnostics.Stopwatch]::StartNew()
+$ErrorActionPreference = "Stop"
+$ProgressPreference = 'SilentlyContinue'
+
+$IsAdmin = (New-Object System.Security.Principal.WindowsPrincipal([System.Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
+if (-not $IsAdmin) {
+    throw "You need to run this from an elevated pwsh prompt"
+}
+
+Write-Host "Setting execution policy" -ForegroundColor Magenta
+Set-ExecutionPolicy RemoteSigned
+
+# Windows PowerShell 5.1 = C:\Users\User\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+# PowerShell 7 = C:\Users\User\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
+
+$PwshProfile = "$HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
+
+Write-Host "Create PowerShell profile if does not exists" -ForegroundColor Magenta
+if (-not (Test-Path $PwshProfile)) {
+    New-Item $PwshProfile -ItemType File -Force
+    Write-Host "`tPowerShell profile created" -ForegroundColor Magenta
+}
+
+Write-Host "Installing WSL stuff and the default 'Ubuntu on Windows' distribution" -ForegroundColor Magenta
+wsl --install
+
+Write-Host "Installing apps via winget" -ForegroundColor Magenta
+winget import -i .\winget-packages.json --accept-package-agreements --accept-source-agreements
+# bring back --disable-interactivity flag once winget version 1.4 is out
+
+# If you are running this on a VM you might get this error:
+# "Error: 0x80370102 The virtual machine could not be started because a required feature is not installed."
+# See https://learn.microsoft.com/en-us/windows/wsl/troubleshooting#error-0x80370102-the-virtual-machine-could-not-be-started-because-a-required-feature-is-not-installed
+# Basically you need to run this on your host machine Set-VMProcessor -VMName [NameOfMyVM] -ExposeVirtualizationExtensions $true
+
+Write-Progress -Activity "Installing chocolatey"
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+Write-Host "`tReloading PowerShell profile" -ForegroundColor Magenta
+. $PwshProfile
+
+Write-Progress -Activity "Installing chocolatey packages"
+$chocoPackages = @(
+    "paint.net"
+    "baretail"
+    "smtp4dev"
+)
+foreach ($package in $chocoPackages) {
+    Write-Host "`tInstalling choco package: $package" -ForegroundColor Magenta
+    choco install -y $package
+    refreshenv
+}
+
+Write-Host "Removing pre-loaded Windows 11(22H2) Apps" -ForegroundColor Magenta
+$unWantedApps = @(
+    "Disney.37853FC22B2CE_6rarf9sa4v8jt"
+    "Microsoft.MicrosoftOfficeHub_8wekyb3d8bbwe"
+    "Microsoft.MicrosoftSolitaireCollection_8wekyb3d8bbwe"
+    "Microsoft.MicrosoftStickyNotes_8wekyb3d8bbwe"
+    "Microsoft.Todos_8wekyb3d8bbwe"
+    "SpotifyAB.SpotifyMusic_zpdnekdrzrea0"
+)
+foreach ($unWantedApp in $unWantedApps) {
+    winget uninstall --id $unWantedApp
+    Write-Host "`tRemoved: '$unWantedApp'" -ForegroundColor Magenta
+}
+
+Write-Host "Installing nuget package provider" -ForegroundColor Magenta
+Install-PackageProvider -Name NuGet -Force
+
+Write-Host "Ensuring PowerShell Gallery is trusted" -ForegroundColor Magenta
+if (-not ((Get-PackageSource -Name "PSGallery").IsTrusted)) {
+    $null = Set-PackageSource -Name "PSGallery" -Trusted
+}
+
+Write-Host "Installing Powershell modules" -ForegroundColor Magenta
+$ModulesToInstall = @(
+    "posh-git"
+    "Terminal-Icons"
+    "DockerCompletion"
+)
+foreach ($Module in $ModulesToInstall) {
+    if ($null -eq (Get-Module -Name $Module -ListAvailable)) {
+        Write-Host ("`tInstalling PowerShell Module - {0}" -f $Module) -ForegroundColor Magenta
+        $null = Install-Module -Name $Module -Force
+    }
+    else {
+        $InstalledVersion = (Get-Module -Name $Module -ListAvailable)[0].version
+        $LatestVersion = (Find-Module -Name $Module)[0].version
+
+        if ($InstalledVersion -lt $LatestVersion) {
+            if ($null -eq (Get-Package -Name $Module -ErrorAction SilentlyContinue)) {
+                Write-Host ("`tForce Installing PowerShell Module - {0}" -f $Module) -ForegroundColor Magenta
+                $null = Install-Module -Name $Module -Force
+            }
+            else {
+                Write-Host ("`tUpdating PowerShell Module - {0}" -f $Module) -ForegroundColor Magenta
+                $null = Update-Module -Name $Module -Force
+            }
+        }
+    }
+}
+
+Write-Host "Adding contents to PwshProfile" -ForegroundColor Magenta
+$Contents = @"
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\atomic.omp.json" | Invoke-Expression
+
+Import-Module -Name DockerCompletion
+Import-Module -Name Terminal-Icons
+Import-Module -Name posh-git
+
+Import-Module -Name PSReadLine
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
+
+Set-Alias -Name "d" -Value "docker"
+Set-Alias -Name "k" -Value "kubectl"
+"@
+Add-Content $PwshProfile $Contents
+
+Write-Host "Setting git aliases" -ForegroundColor Magenta
+git config --global alias.co "checkout"
+git config --global alias.cob "checkout -b"
+git config --global alias.df "diff"
+git config --global alias.ec "config --global -e"
+git config --global alias.f "fetch origin --prune"
+git config --global alias.l "log -n 30 --oneline"
+git config --global alias.lga "log --graph --oneline --all --decorate"
+git config --global alias.st "status"
+
+Write-Host "Setting git credential manager" -ForegroundColor Magenta
+git config --global credential.useHttpPath true
+git config --global credential.helper wincred #https://code.visualstudio.com/docs/remote/troubleshooting#_sharing-git-credentials-between-windows-and-wsl
+
+Write-Host "Setting VS Code as the Git editor" -ForegroundColor Magenta
+git config --global core.editor "code --wait"
+
+git config --global core.fsmonitor=true
+git config --global core.autocrlf=true
+
+# https://cscheng.info/2017/01/26/git-tip-autostash-with-git-pull-rebase.html
+Write-Host "Setting autostash with git pull --rebase" -ForegroundColor Magenta
+git config --global pull.rebase true
+git config --global rebase.autoStash true
+
+$directories = @(
+    "c:\repos"
+    "c:\temp"
+    "c:\games"
+)
+foreach ($directory in $directories) {
+    Write-Host "Creating '$directory' folder" -ForegroundColor Magenta
+    if (-not (Test-Path $directory)) {
+        New-Item $directory -ItemType Directory
+    }
+}
+
+Write-Host "Enabling windows features" -ForegroundColor Magenta
+$features = @(
+    "Microsoft-Hyper-V-All"
+    "Containers"
+    "HypervisorPlatform"
+)
+foreach ($feature in $features) {
+    Write-Host "`tEnabling windows feature: $feature" -ForegroundColor Magenta
+    Enable-WindowsOptionalFeature -Online -NoRestart -FeatureName $feature
+}
+
+$DownloadsDir = "$HOME\Downloads"
+$CascadiaCodeNerdFontZip = "$DownloadsDir\CascadiaCodeNF.zip"
+Invoke-WebRequest -Uri "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/CascadiaCode.zip" -OutFile $CascadiaCodeNerdFontZip
+Expand-Archive -Path $CascadiaCodeNerdFontZip -DestinationPath "$DownloadsDir\CascadiaCodeNF" -Force
+Write-Host "See 'Downloads folder' and install the fonts manually for now, I only need 'Caskaydia Cove Nerd Font Complete Windows Compatible Regular.otf'"
+
+$timer.Stop()
+Write-Host ""
+Write-Host "Script ran for '$($timer.Elapsed.ToString("hh\:mm\:ss"))' on '$env:COMPUTERNAME'" -ForegroundColor Green
+Write-Host "Now do the following:"  -ForegroundColor Yellow
+Write-Host "`t1. Reboot."  -ForegroundColor Yellow
+Write-Host "`t2. Finalized the installation of WSL distros by running them."  -ForegroundColor Yellow
+Write-Host "`t3. Do a windows update."  -ForegroundColor Yellow
+Write-Host "`t4. Update apps from Microsoft Store."  -ForegroundColor Yellow
+Write-Host ""
+Read-Host -Prompt "(*^_^*) All done, press [ENTER] to restart your computer O.O"
+Restart-Computer
